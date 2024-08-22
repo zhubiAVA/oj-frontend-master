70d03596df61f8bd7e13cfed3053bdbd8051ce04
zhubiAVA <1220787961@qq.com>
2024-08-22 22:22:12 +0800

    修改地址为aliyun.xw612.cn 3.0

--- generated/core/OpenAPI.ts
+++ generated/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: 'http://aliyun.xw612.cn:8101',
+    BASE: 'http://aliyun.xw612.cn',
     VERSION: '1.0',
     WITH_CREDENTIALS: true,
     CREDENTIALS: 'include',
